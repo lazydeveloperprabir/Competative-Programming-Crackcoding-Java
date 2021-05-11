16ca90e533b427c5fd05726c1c7248cbeacf63b6
Prabir <lazydeveloperprabir@gmail.com>
2021-05-11 21:24:50 +0530

    First commit

---
+++ Number-Theory/EuclidsAlgorithm.java
@@ -0,0 +1,12 @@
+package number;
+
+public class EuclidsAlgorithm {
+    
+    public static void main(String[] args) {
+        System.out.println("The GCD using Euclid's algorithm :"  + gcd(100,20));
+    }
+    
+    public static int gcd(int a, int b) {
+      return (b == 0 ? a : gcd(b, a % b));
+    }
+}
